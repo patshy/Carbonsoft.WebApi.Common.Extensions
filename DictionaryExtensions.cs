6670f35a601cdfc7b65157bc6cf2754df5780db5
Pete Schmitz <carbonsoft707@gmail.com>
2025-10-21 19:52:58 -0700

    init

---
+++ DictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Carbonsoft.Common.Extensions;
+
+public static class DictionaryExtensions
+{
+    public static bool AreEqual<TKey, TValue>(this IDictionary<TKey, TValue>? first, IDictionary<TKey, TValue>? second)
+        where TValue : class
+    {
+        if (first is null && second is null) return true;
+
+        if (first is null || second is null) return false;
+
+        if (first.Count != second.Count) return false;
+
+        return first.All(x => second.ContainsKey(x.Key) && x.Value == second[x.Key]);
+    }
+}
---
+++ EnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Carbonsoft.Common.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<T> AsNotNull<T>(this IEnumerable<T> sequence)
+    {
+        return sequence ?? [];
+    }
+}
